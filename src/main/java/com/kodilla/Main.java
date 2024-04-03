ab784a429264b3c1e707ad2c0a205e7a02c2f28f
Jarosław Kołacz <jaroslaw.kolacz@op.pl>
2024-04-03 17:41:26 +0200

    OX

---
+++ src/main/java/com/kodilla/Main.java
@@ -0,0 +1,31 @@
+package com.kodilla;
+
+public class Main {
+    public static void main(String[] args) {
+        InputOutputController ioController = new InputOutputController();
+        String playerName = ioController.getPlayerName();
+        char playerSymbol = ioController.getPlayerSymbol();
+        int boardSize = ioController.getBoardSize();
+
+        Player human = new Player(playerName, playerSymbol);
+        Player computer = new Player("Komputer", (playerSymbol == 'X') ? 'O' : 'X');
+
+        Game game = new Game(boardSize);
+        GameService gameService = new GameService(human, computer, game, ioController);
+
+        ioController.displayMessage("Witaj w grze! Rozpoczynamy...");
+        gameService.playGame();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
